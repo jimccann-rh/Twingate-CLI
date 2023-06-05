86062180eb24d8f627c84f66c0b0a5c486fb6e2d
jimb0 <jimccann@redhat.com>
2023-06-05 12:32:18 -0400

    create accounts

--- createusers.py
+++ createusers.py
@@ -5,6 +5,7 @@ import random
 import string
 import re
 import secrets
+import json
 
 # Handy function for GOVC and assume GOVC is on your $PATH
 def govc_runner(command):
@@ -84,6 +85,78 @@ with open(filename, 'r') as f:
     command = ["python3", "./tgcli.py", "-s", session, "user", "create", "-e", newUserEmail, "-r", "MEMBER", "-s", "true", "-f", newFirst, "-l", newLast]
     subprocess.call(command)
 
-command2 = ["python3", "./tgcli.py", "auth", "logout", "-s", session]
-subprocess.call(command2)
+    datainput = ["python3", "./tgcli.py", "-s", session, "group", "list"]
+
+    data = subprocess.check_output(datainput, encoding='UTF-8')
+
+
+    with open("group.txt", "w") as f:
+
+      # Write the variable to the file
+      f.write(str(data))
+
+    # Close the file
+    f.close()
+
+    # Load the JSON file.
+    with open("group.txt", "r") as f:
+      data0 = json.load(f)
+    f.close()
+
+
+    netname = "IBMC-devqe"
+
+    for node in data0[0]:
+      if node['node']['name'] == netname:
+        # Print the node
+        print(node['node']['id'])
+        groupnodeid = node['node']['id']
+
+
+
+    datainput = ["python3", "./tgcli.py", "-s", session, "user", "list"]
+
+    data = subprocess.check_output(datainput, encoding='UTF-8')
+
+
+    with open("email.txt", "w") as f:
+
+      # Write the variable to the file
+      f.write(str(data))
+
+    # Close the file
+    f.close()
+
+    # Load the JSON file.
+    with open("email.txt", "r") as f:
+      data0 = json.load(f)
+    f.close()
+
+
+    email = newUserEmail.replace(" ", "")
+    print(email)
+    for node in data0[0]:
+      if node['node']['email'] == email:
+        # Print the node
+        print(node['node']['id'])
+        emailnodeid = node['node']['id']
+
+
+    command2 = ["python3", "./tgcli.py", "-s", session, "group", "addUsers", "-g", groupnodeid, "-u", emailnodeid]
+    subprocess.call(command2)
+
+
+
+command3 = ["python3", "./tgcli.py", "auth", "logout", "-s", session]
+subprocess.call(command3)
+os.remove("group.txt")
+os.remove("email.txt")
+
+
+
+#animals = ['BlueFly', 'BlackEel', 'RedBoa', 'BlackBat', 'BlackBoa', 'OrangeFox', 'OrangeApe', 'GreenApe', 'WhiteApe', 'PurpleElk', 'RedCow', 'GreenFox', 'YellowFox', 'PinkBoa', 'YellowElk', 'PinkFox', 'GreenBoa', 'RedBat', 'PurpleApe', 'OrangeBat', 'YellowEel', 'OrangeYak', 'RedDog', 'PinkEel', 'PurpleBat', 'OrangeElk', 'BlueBoa', 'OrangeEel', 'GreenCat', 'WhiteDog', 'OrangeCat', 'BlueCat', 'YellowCat', 'GreenCow', 'BlackYak', 'RedCat', 'WhiteFox']
+
+# Print the list
+#for animal in animals:
+#   subprocess.call(["python3", "./tgcli.py", "auth", "logout", "-s", animal])
 
